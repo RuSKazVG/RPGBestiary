6098ac3c9eebe0649a82e637f09271507fa5540b
RuSKazVG <rkazakov2006@gmail.com>
2025-03-19 19:17:59 +0400

    initial push

--- src/main/java/me/ruskaz/rpgbestiary/api/ConfigManager.java
+++ src/main/java/me/ruskaz/rpgbestiary/api/ConfigManager.java
@@ -13,9 +13,6 @@ import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 import java.io.IOException;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
